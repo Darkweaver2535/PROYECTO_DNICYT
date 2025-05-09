82efdf4a74a5df52a36e1444b36b110e9a32c28c
Alvaro Encinas <alvaroencinas@Alvaros-MacBook-Pro.local>
2025-05-09 14:16:22 -0400

    agregando requeriments.txt y gitignore

--- PROYECTO_ACTIVOS_INDUSTRIALES/settings.py
+++ PROYECTO_ACTIVOS_INDUSTRIALES/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.capacitacion',
+    'apps.equipos',
+    'apps.mantenimiento',
+    'apps.seguridad',
+    'apps.usuarios'
 ]
 
 MIDDLEWARE = [
@@ -103,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-BO'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/La_Paz'
 
 USE_I18N = True
 
