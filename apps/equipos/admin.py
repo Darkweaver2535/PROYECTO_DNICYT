e9811b83b95589ee92ee194c5369876498468aba
Alvaro Encinas <alvaroencinas@Alvaros-MacBook-Pro.local>
2025-05-09 14:16:22 -0400

    agregando requeriments.txt y gitignore

--- apps/equipos/admin.py
+++ apps/equipos/admin.py
@@ -1,3 +1,9 @@
+# equipos/admin.py
 from django.contrib import admin
+from .models import Seccion
 
-# Register your models here.
+@admin.register(Seccion)
+class SeccionAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'identificador', 'responsable')
+    search_fields = ('nombre', 'identificador')
+    list_filter = ('ubicacion',)
--- apps/equipos/models.py
+++ apps/equipos/models.py
@@ -1,3 +1,16 @@
+# equipos/models.py
 from django.db import models
 
-# Create your models here.
+class Seccion(models.Model):
+    nombre = models.CharField(max_length=100)
+    identificador = models.CharField(max_length=50)
+    ubicacion = models.CharField(max_length=200)
+    responsable = models.CharField(max_length=100)
+    colaborador = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name = "Sección"
+        verbose_name_plural = "Secciones"
