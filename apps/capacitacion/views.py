68cc5c7f371cda5f4f638024bf36ec29efd3a817
Alvaro Encinas <AlvaroEncinas26@gmail.com>
2025-06-02 21:39:46 -0400

    actualizando las bases

--- apps/capacitacion/views.py
+++ apps/capacitacion/views.py
@@ -1,3 +1,8 @@
+from django.contrib import messages
 from django.shortcuts import render
+
+def mi_vista(request):
+    messages.success(request, '¡Inicio de sesión exitoso!')
+    messages.error(request, 'Usuario o contraseña incorrectos')
 
 # Create your views here.
